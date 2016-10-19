8660b345de5df302290754e3a55e7f88fff3e394
Liam Taylor <liam.taylor888@googlemail.com>
2016-10-19 12:37:03 +0100

    Built multiple tagging functionality: tests pass. Added command-lin auto update

--- Gemfile
+++ Gemfile
@@ -1,11 +1,12 @@
 source "https://rubygems.org"
 
-ruby '2.3.1'
+ruby '2.3.1 '
 
 gem 'data_mapper'
 gem 'dm-postgres-adapter'
 gem 'sinatra'
 gem 'database_cleaner'
+gem 'rake'
 
 group :test do
   gem 'capybara'
--- app/app.rb
+++ app/app.rb
@@ -13,20 +13,18 @@ class BookmarkManager < Sinatra::Base
   end
 
   post '/links' do
-    sausages = Link.new(url: params[:url], title: params[:title])
-    tag = Tag.first_or_create(name: params[:tags])
-    sausages.tags << tag
-    sausages.save
-    redirect to('/links')
-  end
-
-  get '/tags/:name' do
     link = Link.create(url: params[:url], title: params[:title])
     params[:tags].split.each do |tag|
       link.tags << Tag.create(name: tag)
     end
     link.save
     redirect to('/links')
+  end
+
+  get '/tags/:name' do
+    tag = Tag.first(name: params[:filter])
+    @links = tag ? tag.links : []
+    erb :'links/index'
   end
 
 run! if app_file == $0
