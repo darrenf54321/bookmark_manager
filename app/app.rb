683c19c2a20cc1b26c796c3e9e4658195e483c88
Darrenf <darrenf54321@gmail.com>
2016-10-19 12:09:29 +0100

    Handover

--- app/app.rb
+++ app/app.rb
@@ -21,9 +21,12 @@ class BookmarkManager < Sinatra::Base
   end
 
   get '/tags/:name' do
-    tag = Tag.first(name: params[:filter])
-    @links = tag ? tag.links : []
-    erb :'links/index'
+    link = Link.create(url: params[:url], title: params[:title])
+    params[:tags].split.each do |tag|
+      link.tags << Tag.create(name: tag)
+    end
+    link.save
+    redirect to('/links')
   end
 
 run! if app_file == $0
--- spec/filter_by_link_spec.rb
+++ spec/filter_by_link_spec.rb
@@ -16,4 +16,15 @@ feature 'Tag filter' do
       expect(page).not_to have_content('Title: Makers Academy')
     end
   end
+
+  scenario 'Multiple tags' do
+    # Link.create(url: 'http://www.makersacademy.com', title: 'Makers Academy', tags: [Tag.first_or_create(name: 'education')])
+    visit '/links/new'
+    fill_in 'title', with: 'Bubble Smith'
+    fill_in 'url', with: 'http://http://www.deliaonline.com'
+    fill_in 'tags', with: 'Bubbles News Food'
+    click_button 'Add'
+    link = Link.first
+    expect(link.tags.map(&:name)).to include('Bubbles', 'News', 'Food')
+end
 end
