24718368ae5676ac4ec3bef9241d4e99d9cab8a2
Darrenf <darrenf54321@gmail.com>
2016-10-20 11:32:16 +0100

    signup feature test, failing

---
+++ spec/features/register_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+feature "user sign-up" do
+  scenario "the user registers with a password" do
+    visit "/"
+    click_button "Register"
+    expect(current_path).to eq '/register'
+    fill_in :email, with: 'batmam@hotmail.com'
+    fill_in :password, with: 'robin'
+    click_button "ok"
+    expect(page).to have_content "Welcome batman@hotmail.com"
+
+  end
+end
