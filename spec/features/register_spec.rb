ecc6debc558df337a116429f075195afe4d331e9
Darrenf <darrenf54321@gmail.com>
2016-10-21 14:50:37 +0100

    Existing email regsitration prevented

--- spec/features/register_spec.rb
+++ spec/features/register_spec.rb
@@ -9,14 +9,26 @@ feature "user sign-up" do
   scenario 'with a password that does not match' do
     expect { sign_up(email: "cat@hotmail.com", password: "123", password_confirmation: "456")}.not_to change(User, :count)
     expect(current_path).to eq('/register')
-    expect(page).to have_content 'Password and confirmation password do not match'
+    expect(page).to have_content 'Password does not match the confirmation'
   end
 
   scenario "I can't sign up without an email address" do
     expect { sign_up(email: nil, password: "123", password_confirmation: "123") }.not_to change(User, :count)
+    expect(current_path).to eq('/register')
+    expect(page).to have_content('Email must not be blank')
   end
 
   scenario "I can't sign up with an invalid email address" do
     expect { sign_up(email: "invalid@email", password: "123", password_confirmation: "123") }.not_to change(User, :count)
+    expect(current_path).to eq('/register')
+    expect(page).to have_content('Email has an invalid format')
   end
+
+  scenario 'I cannot sign up with an existing email' do
+    sign_up(email: "cat@hotmail.com", password: "123", password_confirmation: "123")
+    expect { sign_up(email: "cat@hotmail.com", password: "123", password_confirmation: "123")}.not_to change(User, :count)
+    expect(current_path).to eq('/register')
+    expect(page).to have_content('Email is already taken')
+  end
+
 end
